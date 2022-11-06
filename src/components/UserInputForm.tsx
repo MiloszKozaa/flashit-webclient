2d61a235163a062dd2248a4a90f0ce93336581b1
Amen't <milosz.koza12@gmail.com>
2022-11-06 17:35:04 +0100

    created show password button

--- src/components/UserInputForm.tsx
+++ src/components/UserInputForm.tsx
@@ -8,24 +8,46 @@ const Input = ({
   onChange,
   ...inputProps
 }: any) => {
-  const [focused, setFocused] = useState(false);
+  const [focused, focusedSet] = useState(false);
+  const [showPassword, showPasswordSet] = useState(false);
 
   const handleFocus = () => {
-    setFocused(true);
+    focusedSet(true);
   };
 
   return (
     <div className='input_wrapper'>
-      <img src={`${process.env.PUBLIC_URL}/icon/${icon}.svg`} alt='' />
+      <img
+        src={`${process.env.PUBLIC_URL}/icon/${icon}.svg`}
+        className='input_image'
+      />
       <input
         {...inputProps}
+        type={
+          inputProps.type === 'password'
+            ? !showPassword
+              ? 'password'
+              : 'text'
+            : inputProps.type
+        }
         onChange={onChange}
         onBlur={handleFocus}
         onFocus={() =>
-          inputProps.name === 'confirmPassword' && setFocused(true)
+          inputProps.name === 'confirmPassword' && focusedSet(true)
         }
         focused={focused.toString()}
       />
+      {inputProps.type === 'password' ? (
+        <img
+          src={
+            !showPassword
+              ? `${process.env.PUBLIC_URL}/icon/eye.svg`
+              : `${process.env.PUBLIC_URL}/icon/eyeHide.svg`
+          }
+          className='icon'
+          onClick={() => showPasswordSet(!showPassword)}
+        />
+      ) : null}
       <span className='line'></span>
       <span className='error_message'>{errorMessage}</span>
     </div>
